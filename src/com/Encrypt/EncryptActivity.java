d2c9ad77d7bd2504e9dc89742710baca727d5a3a
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2010-11-04 16:30:47 -0400

    First working edition.  Hardcoded to encrypted in AES a hardcoded plaintext string with another hardcoded string acting as the seed for the encryption algorithm.  This results in the encrypted string being displayed back to the user as a text.
    To do:
    *Write a function to decrypt the string.
    *Display the plaintext string, the encrypted string, and then the decrypted string to make
    sure that the encoding/decoding algorithm works.
    *Create a function that encrypts a file, and a function that encrypts a string.
    
     On branch experimental
      Changes to be committed:
    	modified:   src/com/Encrypt/EncryptActivity.java
     Untracked files:
    	.classpath
    	.project
    	bin/
    	default.properties
    	gen/
    	res/drawable-hdpi/
    	res/drawable-ldpi/
    	res/drawable-mdpi/

--- src/com/Encrypt/EncryptActivity.java
+++ src/com/Encrypt/EncryptActivity.java
@@ -13,11 +13,16 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class EncryptActivity extends Activity {
     private static final String LOG_TAG = EncryptActivity.class.getName();
@@ -28,41 +33,60 @@ public class EncryptActivity extends Activity {
         setContentView(R.layout.main);
         
         EditText fileNameEdit = (EditText) findViewById(R.id.FileNameEdit);
+        Button browseButton = (Button) findViewById(R.id.BrowseButton);
         Spinner EncryptionSelect = (Spinner) findViewById(R.id.EncryptSelect);
+        Button EncryptItButton = (Button) findViewById(R.id.EncryptItButton);
+        
         ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.EncryptSelectOptions, 
             android.R.layout.simple_spinner_item);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         EncryptionSelect.setAdapter(spinnerAdapter);
         
-        String seed = "This is whatever passphrase is used to protect this content.";
-        String plainText = "It's a secret to everybody (not)";
-        String EncryptionType = "AES";
+        final Context encrytStringActivity = this;
+        final String encryptionType = EncryptionSelect.getSelectedItem().toString();
+        EncryptItButton.setOnClickListener(new OnClickListener(){
+          @Override
+          public void onClick(View encryptView) {
+            String result = encryptString(encryptionType);
+            Toast.makeText(encrytStringActivity, result, Toast.LENGTH_LONG).show();
+          }
+        });
+    }
+    
+    private String encryptString(String encryptionType){
+      String seed = "This is whatever passphrase is used to protect this content.";
+      String plainText = "It's a secret to everybody (not)";
+      
+      String result = " ";
+      
+      try{
+        KeyGenerator keyGen = KeyGenerator.getInstance(encryptionType);
+      //Use SHA1 algorithm
+        SecureRandom secRand = SecureRandom.getInstance("SHA1PRNG");
+        secRand.setSeed(seed.getBytes());
         
-        try{
-          KeyGenerator keyGen = KeyGenerator.getInstance(EncryptionType);
-        //Use SHA1 algorithm
-          SecureRandom secRand = SecureRandom.getInstance("SHA1PRNG");
-          secRand.setSeed(seed.getBytes());
-          
-          keyGen.init(128, secRand);
-          SecretKey key = keyGen.generateKey();
-          byte[] rawKey = key.getEncoded();
-          
-          SecretKeySpec skeySpec = new SecretKeySpec(rawKey, EncryptionType);
-          Cipher dCipher = Cipher.getInstance(EncryptionType);
-          dCipher.init(Cipher.ENCRYPT_MODE, skeySpec);
-          
-          byte[] encrypted = dCipher.doFinal(plainText.getBytes());
-        }catch(NoSuchAlgorithmException nsae){
-          Log.e(LOG_TAG, "There is no algorithm that coresponds to " + EncryptionType + ".");
-        } catch(NoSuchPaddingException nspe){
-          Log.e(LOG_TAG, nspe.getMessage());
-        } catch(InvalidKeyException ike){
-          Log.e(LOG_TAG, ike.getMessage());
-        } catch(BadPaddingException bpe){
-          Log.e(LOG_TAG, bpe.getMessage());
-        } catch(IllegalBlockSizeException ibse){
-          Log.e(LOG_TAG, ibse.getMessage());
-        }
+        keyGen.init(128, secRand);
+        SecretKey key = keyGen.generateKey();
+        byte[] rawKey = key.getEncoded();
+        
+        SecretKeySpec skeySpec = new SecretKeySpec(rawKey, encryptionType);
+        Cipher dCipher = Cipher.getInstance(encryptionType);
+        dCipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+        
+        byte[] encrypted = dCipher.doFinal(plainText.getBytes());
+        result = encrypted.toString();
+      }catch(NoSuchAlgorithmException nsae){
+        Log.e(LOG_TAG, "There is no algorithm that coresponds to " + encryptionType + ".");
+      } catch(NoSuchPaddingException nspe){
+        Log.e(LOG_TAG, nspe.getMessage());
+      } catch(InvalidKeyException ike){
+        Log.e(LOG_TAG, ike.getMessage());
+      } catch(BadPaddingException bpe){
+        Log.e(LOG_TAG, bpe.getMessage());
+      } catch(IllegalBlockSizeException ibse){
+        Log.e(LOG_TAG, ibse.getMessage());
+      }
+      
+      return result;
     }
 }
