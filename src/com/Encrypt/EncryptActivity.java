b92527e9c55dc03b351dc6b800d9f50f76b0b5d5
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2010-11-14 20:39:17 -0500

    Made a primary page to direct user to string encrypting activity and to file encrypting activity. String encrypt activity is completed.
    Need to do:
    *File encrypt activity.
    
     On branch experimental
     Changes to be committed:
    	modified:   AndroidManifest.xml
    	renamed:    res/layout/main.xml -> res/layout/encrypt_string.xml
    	modified:   res/layout/main.xml
    	modified:   res/values/strings.xml
    	modified:   src/com/Encrypt/EncryptActivity.java
    	renamed:    src/com/Encrypt/EncryptActivity.java -> src/com/Encrypt/EncryptStringActivity.java
     Untracked files:
    	.classpath
    	.project
    	bin/
    	default.properties
    	gen/
    	res/drawable-hdpi/
    	res/drawable-ldpi/
    	res/drawable-mdpi/

--- src/com/Encrypt/EncryptActivity.java
+++ src/com/Encrypt/EncryptActivity.java
@@ -1,27 +1,12 @@
 package com.Encrypt;
 
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.KeyGenerator;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
-
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.Toast;
 
 public class EncryptActivity extends Activity {
     private static final String LOG_TAG = EncryptActivity.class.getName();
@@ -30,75 +15,18 @@ public class EncryptActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        final EditText fileNameEdit = (EditText) findViewById(R.id.FileNameEdit);
-        Button browseButton = (Button) findViewById(R.id.BrowseButton);
-        final Spinner encryptionSelect = (Spinner) findViewById(R.id.EncryptSelect);
-        Button encryptItButton = (Button) findViewById(R.id.EncryptItButton);
-        final EditText seedEdit = (EditText) findViewById(R.id.seedEdit);
-        
-        ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.EncryptSelectOptions, 
-            android.R.layout.simple_spinner_item);
-        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        encryptionSelect.setAdapter(spinnerAdapter);
+       
+        Button encryptStringButton = (Button) findViewById(R.id.EncryptStringButton);
+        Button encryptFileButton = (Button) findViewById(R.id.EncryptFileButton);
         
         final Context encryptStringActivity = this;
         
-        encryptItButton.setOnClickListener(new OnClickListener(){
+        encryptStringButton.setOnClickListener(new OnClickListener(){
           @Override
           public void onClick(View encryptView) {
-            String fileName = fileNameEdit.getEditableText().toString();
-            String encryptionType = encryptionSelect.getSelectedItem().toString();
-            String seed = seedEdit.getEditableText().toString();
-            Cipher encCipher;
-            Cipher decCipher;
-            try{
-              SecretKey key = KeyGenerator.getInstance(encryptionType).generateKey();
-              encCipher = Cipher.getInstance(encryptionType);
-              decCipher = Cipher.getInstance(encryptionType);
-              encCipher.init(Cipher.ENCRYPT_MODE, key);
-              decCipher.init(Cipher.DECRYPT_MODE, key);
-              byte[] encrypted = encryptString(fileName, encCipher);
-              String decrypted = decryptString(encrypted, decCipher);
-              Toast.makeText(encryptStringActivity, "This is the encrypted string " + new String(encrypted) +
-                  "\nThis is the decrypted string " + decrypted, Toast.LENGTH_SHORT).show();
-            } catch (NoSuchPaddingException e) {
-              Log.e(LOG_TAG, e.getMessage());
-            } catch (NoSuchAlgorithmException e) { 
-              Log.e(LOG_TAG, e.getMessage());
-            } catch (InvalidKeyException e) { 
-              Log.e(LOG_TAG, e.getMessage());
-            } 
+            Intent encStrIntent = new Intent(EncryptActivity.this, EncryptStringActivity.class);
+            startActivity(encStrIntent);
           }
         });
-    }
-    
-    private byte[] encryptString(String toEncrypt, Cipher encCipher){
-      try{
-        byte[] utf8 = toEncrypt.getBytes("UTF8");
-        byte[] enc = encCipher.doFinal(utf8);
-        return enc; 
-      } catch(BadPaddingException e){
-        Log.e(LOG_TAG, e.getMessage());
-      } catch(UnsupportedEncodingException e){
-        Log.e(LOG_TAG, e.getMessage());
-      } catch(IllegalBlockSizeException e){
-        Log.e(LOG_TAG, e.getMessage());
-      }
-      return null;
-    }
-    
-    private String decryptString(byte[] toDecrypt, Cipher decCipher){
-      try{
-        byte[] decrypt = decCipher.doFinal(toDecrypt);
-        return new String(decrypt, "UTF8");
-      } catch(BadPaddingException e){
-        Log.e(LOG_TAG, e.getMessage());
-      } catch(IllegalBlockSizeException e){
-        Log.e(LOG_TAG, e.getMessage());
-      } catch(UnsupportedEncodingException e){
-        Log.e(LOG_TAG, e.getMessage());
-      }
-      return null;
     }
 }
