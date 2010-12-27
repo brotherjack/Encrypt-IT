01ed0edd75b3b201241e54bcd103daf1a3117283
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2010-12-27 17:30:05 -0500

    Changed the name of the main activity to "HomeActivity" to make it stand out more easily in logs and when working on it within the project.  Working on the output file name.  Need to grab the last branch in the path and add it to the key directory forming <key directory>/<key name>.key.  Working on this in the function saveKey.

--- src/com/encryptit/app/EncryptFileActivity.java
+++ src/com/encryptit/app/EncryptFileActivity.java
@@ -9,14 +9,13 @@ import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Random;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
-
-import com.encryptit.app.R;
 
 import android.app.Activity;
 import android.content.Context;
@@ -33,240 +32,272 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 public class EncryptFileActivity extends Activity {
-  private static final String LOG_TAG = EncryptFileActivity.class.getName();
-  private static SharedPreferences mPreferences;
-  private static final int RETURN_PATH_TO_LOAD = 83;
-  private final String SELECTED_PATH = "selected.path";
-  private static Button mBrowseButton;
-  private static EditText mFileNameEdit;
+	private static final String LOG_TAG = EncryptFileActivity.class.getName();
+	private static SharedPreferences mPreferences;
+	private static final int RETURN_PATH_TO_LOAD = 83;
+	private final String SELECTED_PATH = "selected.path";
+	private static Button mBrowseButton;
+	private static EditText mFileNameEdit;
 
-  /** Called when the activity is first created. */
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.encrypt_file);
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.encrypt_file);
 
-    mFileNameEdit = (EditText) findViewById(R.id.FileNameEdit);
-    final Spinner encryptionSelect = (Spinner) findViewById(R.id.EncryptSelect);
-    mBrowseButton = (Button) findViewById(R.id.BrowseButton);
-    Button encryptItButton = (Button) findViewById(R.id.EncryptItButton);
-    final EditText seedEdit = (EditText) findViewById(R.id.seedEdit);
+		mFileNameEdit = (EditText) findViewById(R.id.FileNameEdit);
+		final Spinner encryptionSelect = (Spinner) findViewById(R.id.EncryptSelect);
+		mBrowseButton = (Button) findViewById(R.id.BrowseButton);
+		Button encryptItButton = (Button) findViewById(R.id.EncryptItButton);
+		final EditText seedEdit = (EditText) findViewById(R.id.seedEdit);
 
-    ArrayAdapter<CharSequence> spinnerAdapter =
-        ArrayAdapter.createFromResource(this, R.array.EncryptSelectOptions,
-            android.R.layout.simple_spinner_item);
-    spinnerAdapter
-        .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    encryptionSelect.setAdapter(spinnerAdapter);
+		ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter
+				.createFromResource(this, R.array.EncryptSelectOptions,
+						android.R.layout.simple_spinner_item);
+		spinnerAdapter
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		encryptionSelect.setAdapter(spinnerAdapter);
 
-    final Context encryptFileActivity = this;
+		final Context encryptFileActivity = this;
 
-    encryptItButton.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View encryptView) {
-        String fileName = mFileNameEdit.getEditableText().toString();
-        String encryptionType = encryptionSelect.getSelectedItem().toString();
-        String seed = seedEdit.getEditableText().toString();
+		encryptItButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View encryptView) {
+				String fileName = mFileNameEdit.getEditableText().toString();
+				String encryptionType = encryptionSelect.getSelectedItem()
+						.toString();
+				String seed = seedEdit.getEditableText().toString();
 
-        Context thisContext = encryptFileActivity.getApplicationContext();
-        mPreferences = thisContext.getSharedPreferences("User Preferences", 0);
+				Context thisContext = encryptFileActivity
+						.getApplicationContext();
+				mPreferences = thisContext.getSharedPreferences(
+						"User Preferences", 0);
+				String fileOut = "";
 
-        try {
-          String keyDir = mPreferences.getString("keyDir", null);
-          String encryptedDir = mPreferences.getString("encryptedDir", null);
+				try {
+					String keyDir = mPreferences.getString("keyDir", null);
+					String encryptedDir = mPreferences.getString(
+							"encryptedDir", null);
+					String newFileName;
 
-          if ((keyDir == null) || (encryptedDir == null)) {
-            throw new KeyGenFailException(
-                KeyGenFailException.failureTypes.DATABASE_ERROR);
-          } else if (fileName.compareTo("") == 0) {
-            throw new KeyGenFailException(
-                KeyGenFailException.failureTypes.EMPTY_FIELD);
-          }
+					if ((keyDir == null) || (encryptedDir == null)) {
+						throw new KeyGenFailException(
+								KeyGenFailException.failureTypes.DATABASE_ERROR);
+					} else if (fileName.compareTo("") == 0) {
+						throw new KeyGenFailException(
+								KeyGenFailException.failureTypes.EMPTY_FIELD);
+					}
 
-          String fileIn = "/mnt/sdcard/usefulcmds.txt";
-          String fileOut = encryptedDir + "/" + fileName;
-          String keyFileName = keyDir + "/" + fileName;
+					String fileIn = fileName;
+					newFileName = makeRandomFileName() + ".ef";
+					fileOut = encryptedDir + "/" + newFileName;
+					String keyFileName = keyDir + "/" + fileName;
 
-          SecretKeySpec key = makeKey(keyFileName, seed);
-          if (key == null)
-            throw new KeyGenFailException(
-                KeyGenFailException.failureTypes.KEY_NULL);
-          else
-            saveKey(key, keyFileName);
+					SecretKeySpec key = makeKey(keyFileName, seed);
+					if (key == null)
+						throw new KeyGenFailException(
+								KeyGenFailException.failureTypes.KEY_NULL);
+					else
+						saveKey(key, keyDir, fileName);
 
-          encryptFile(fileIn, fileOut, encryptionType, key);
-        } catch (KeyGenFailException e) {
-          String msg = null;
-          if (e.fail == KeyGenFailException.failureTypes.KEY_NULL) {
-            msg = "Was unable to generate a key!";
-            Log.e(LOG_TAG, msg);
-            Toast.makeText(encryptFileActivity, msg, Toast.LENGTH_SHORT).show();
-          }
-          if (e.fail == KeyGenFailException.failureTypes.DATABASE_ERROR) {
-            msg = "Corrupted value for directory in databse.";
-            Log.e(LOG_TAG, msg);
-            Toast.makeText(encryptFileActivity, msg, Toast.LENGTH_SHORT).show();
-          }
-          if (e.fail == KeyGenFailException.failureTypes.EMPTY_FIELD) {
-            msg = "Please enter a file name to encrypt.";
-            Toast.makeText(encryptFileActivity, msg, Toast.LENGTH_SHORT).show();
-          }
-        }
-      }
-    });
+					encryptFile(fileIn, fileOut, encryptionType, key);
+				} catch (KeyGenFailException e) {
+					String msg = null;
+					if (e.fail == KeyGenFailException.failureTypes.KEY_NULL) {
+						msg = "Was unable to generate a key!";
+						Log.e(LOG_TAG, msg);
+						Toast.makeText(encryptFileActivity, msg,
+								Toast.LENGTH_SHORT).show();
+					}
+					if (e.fail == KeyGenFailException.failureTypes.DATABASE_ERROR) {
+						msg = "Corrupted value for directory in databse.";
+						Log.e(LOG_TAG, msg);
+						Toast.makeText(encryptFileActivity, msg,
+								Toast.LENGTH_SHORT).show();
+					}
+					if (e.fail == KeyGenFailException.failureTypes.EMPTY_FIELD) {
+						msg = "Please enter a file name to encrypt.";
+						Toast.makeText(encryptFileActivity, msg,
+								Toast.LENGTH_SHORT).show();
+					}
+					Toast.makeText(
+							encryptFileActivity,
+							"Successfuly encrypted \"" + fileName + "\" as \""
+									+ " \"" + fileOut + " \" ",
+							Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
 
-    mBrowseButton.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View encryptView) {
-        Intent fileViewIntent =
-            new Intent(EncryptFileActivity.this, FileListActivity.class);
-        startActivityForResult(fileViewIntent, RETURN_PATH_TO_LOAD);
-      }
-    });
-  }
+		mBrowseButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View encryptView) {
+				Intent fileViewIntent = new Intent(EncryptFileActivity.this,
+						FileListActivity.class);
+				startActivityForResult(fileViewIntent, RETURN_PATH_TO_LOAD);
+			}
+		});
+	}
 
-  @Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    if (requestCode == RETURN_PATH_TO_LOAD) {
-      switch (resultCode) {
-        case (Activity.RESULT_OK): // If path was returned successfully
-          if (resultCode == Activity.RESULT_OK) {
-            if (data.hasExtra(SELECTED_PATH)) {
-              Bundle stuff = data.getExtras();
-              String path = stuff.getString(SELECTED_PATH);
-              mFileNameEdit.setText(path);
-              Log.i(LOG_TAG, "Placed path named \"" + path
-                  + "\" into FileNameEdit, EditText.");
-            }
-          }
-          break;
-      }
-    }
-  }
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == RETURN_PATH_TO_LOAD) {
+			switch (resultCode) {
+			case (Activity.RESULT_OK): // If path was returned successfully
+				if (resultCode == Activity.RESULT_OK) {
+					if (data.hasExtra(SELECTED_PATH)) {
+						Bundle stuff = data.getExtras();
+						String path = stuff.getString(SELECTED_PATH);
+						mFileNameEdit.setText(path);
+						Log.i(LOG_TAG, "Placed path named \"" + path
+								+ "\" into FileNameEdit, EditText.");
+					}
+				}
+				break;
+			}
+		}
+	}
 
-  private SecretKeySpec makeKey(String fileName, String seed) {
-    try {
-      SecureRandom secRand = SecureRandom.getInstance("SHA1PRNG");
-      secRand.setSeed(seed.getBytes("UTF-8"));
+	private String makeRandomFileName() {
+		// Create random value generator
+		Random gen = new Random(System.currentTimeMillis());
 
-      byte[] key = (fileName + secRand.toString()).getBytes("UTF-8");
-      MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
+		// A character in java is two bytes, so this will be a string between 1
+		// and 15 characters long
+		int stringSize = 1 + gen.nextInt(15) * 2;
+		byte[] buf = new byte[stringSize];
 
-      key = sha1.digest(key);
-      // Reduce key to 128 bytes
-      byte[] shorterKey = new byte[16];
-      for (int i = 0; i < 16; i++)
-        shorterKey[i] = key[i];
+		gen.setSeed(System.currentTimeMillis()); // Set to new seed
+		gen.nextBytes(buf);
+		return new String(buf.toString());
+	}
 
-      return new SecretKeySpec(shorterKey, "AES");
-    } catch (NoSuchAlgorithmException e) {
-      Log.e(LOG_TAG, e.getMessage());
-    } catch (UnsupportedEncodingException e) {
-      Log.e(LOG_TAG, e.getMessage());
-    }
-    return null;
-  }
+	private SecretKeySpec makeKey(String fileName, String seed) {
+		try {
+			SecureRandom secRand = SecureRandom.getInstance("SHA1PRNG");
+			secRand.setSeed(seed.getBytes("UTF-8"));
 
-  private void saveKey(SecretKeySpec key, String fileName) {
-    FileOutputStream output = null;
-    try {
-      File fileOut = new File(fileName + ".key");
+			byte[] key = (fileName + secRand.toString()).getBytes("UTF-8");
+			MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
 
-      output = new FileOutputStream(fileOut);
+			key = sha1.digest(key);
+			// Reduce key to 128 bytes
+			byte[] shorterKey = new byte[16];
+			for (int i = 0; i < 16; i++)
+				shorterKey[i] = key[i];
 
-      output.write(key.getEncoded());
-    } catch (IOException e) {
-      Log.e(LOG_TAG, e.getMessage());
-    } finally {
-      if (output != null) try {
-        output.close();
-      } catch (IOException e) {
-        Log.e(LOG_TAG, "Cannot close output stream!");
-        e.printStackTrace();
-      }
-    }
-  }
+			return new SecretKeySpec(shorterKey, "AES");
+		} catch (NoSuchAlgorithmException e) {
+			Log.e(LOG_TAG, e.getMessage());
+		} catch (UnsupportedEncodingException e) {
+			Log.e(LOG_TAG, e.getMessage());
+		}
+		return null;
+	}
 
-  private void encryptFile(String fileInName, String fileOutName,
-      String encryptionType, SecretKeySpec key) {
-    FileInputStream input = null;
-    FileOutputStream output = null;
-    Cipher encCipher = null;
-    try {
-      encCipher = Cipher.getInstance(encryptionType);
-      encCipher.init(Cipher.ENCRYPT_MODE, key);
-      File fileIn = new File(fileInName);
-      if (fileIn.length() > Integer.MAX_VALUE)
-        throw new FileTooBigException("File\'" + fileInName
-            + "\' is too large too encrypt.");
+	private void saveKey(SecretKeySpec key, String keyDir, String filePath) {
+		FileOutputStream output = null;
+		try {
+			
+			File fileOut = new File(filePath + ".key");
 
-      input = new FileInputStream(fileIn);
-      byte[] plainText = new byte[(int) fileIn.length()];
-      input.read(plainText);
+			output = new FileOutputStream(fileOut);
 
-      byte[] encrypted = encCipher.doFinal(plainText);
+			output.write(key.getEncoded());
+		} catch (IOException e) {
+			Log.e(LOG_TAG, e.getMessage());
+		} finally {
+			if (output != null)
+				try {
+					output.close();
+				} catch (IOException e) {
+					Log.e(LOG_TAG, "Cannot close output stream!");
+					e.printStackTrace();
+				}
+		}
+	}
 
-      output = new FileOutputStream(new File(fileOutName + ".enc"));
-      output.write(encrypted);
+	private void encryptFile(String fileInName, String fileOutName,
+			String encryptionType, SecretKeySpec key) {
+		FileInputStream input = null;
+		FileOutputStream output = null;
+		Cipher encCipher = null;
+		try {
+			encCipher = Cipher.getInstance(encryptionType);
+			encCipher.init(Cipher.ENCRYPT_MODE, key);
+			File fileIn = new File(fileInName);
+			if (fileIn.length() > Integer.MAX_VALUE)
+				throw new FileTooBigException("File\'" + fileInName
+						+ "\' is too large too encrypt.");
 
-      Toast.makeText(this, "Encrypted \'" + fileInName + "\'.",
-          Toast.LENGTH_SHORT).show();
-    } catch (BadPaddingException e) {
-      Log.e(LOG_TAG, e.getMessage());
-    } catch (UnsupportedEncodingException e) {
-      Log.e(LOG_TAG, e.getMessage());
-    } catch (IllegalBlockSizeException e) {
-      Log.e(LOG_TAG, e.getMessage());
-    } catch (FileTooBigException e) {
-      Log.e(LOG_TAG, e.getMessage());
-      Toast.makeText(this, "Was unable to encrypt file \'" + fileInName
-          + "\' is too large too encrypt.", Toast.LENGTH_SHORT);
-    } catch (IOException ioe) {
-      Log.e(LOG_TAG, ioe.getMessage());
-      Toast.makeText(this,
-          "Was unable to encrypt file \'" + fileInName + "\'.",
-          Toast.LENGTH_SHORT);
-    } catch (NoSuchAlgorithmException e) {
-      Log.e(LOG_TAG, e.getMessage());
-    } catch (NoSuchPaddingException e) {
-      Log.e(LOG_TAG, e.getMessage());
-    } catch (InvalidKeyException e) {
-      Log.e(LOG_TAG, e.getMessage());
-    } finally {
-      if (input != null) try {
-        input.close();
-      } catch (IOException e) {
-        Log.e(LOG_TAG, "Cannot close input stream!");
-        e.printStackTrace();
-      }
-      if (output != null) try {
-        output.close();
-      } catch (IOException e) {
-        Log.e(LOG_TAG, "Cannot close output stream!");
-        e.printStackTrace();
-      }
-    }
-  }// End encryptFile
+			input = new FileInputStream(fileIn);
+			byte[] plainText = new byte[(int) fileIn.length()];
+			input.read(plainText);
 
-  private class FileTooBigException extends Exception {
-    static final long serialVersionUID = 1877;
+			byte[] encrypted = encCipher.doFinal(plainText);
 
-    FileTooBigException(String msg) {
-      super(msg);
-    }
-  }
+			output = new FileOutputStream(new File(fileOutName + ".enc"));
+			output.write(encrypted);
 
-  private static class KeyGenFailException extends Exception {
-    static final long serialVersionUID = 1878;
+			Toast.makeText(this, "Encrypted \'" + fileInName + "\'.",
+					Toast.LENGTH_SHORT).show();
+		} catch (BadPaddingException e) {
+			Log.e(LOG_TAG, e.getMessage());
+		} catch (UnsupportedEncodingException e) {
+			Log.e(LOG_TAG, e.getMessage());
+		} catch (IllegalBlockSizeException e) {
+			Log.e(LOG_TAG, e.getMessage());
+		} catch (FileTooBigException e) {
+			Log.e(LOG_TAG, e.getMessage());
+			Toast.makeText(this, "Was unable to encrypt file \'" + fileInName
+					+ "\' is too large too encrypt.", Toast.LENGTH_SHORT);
+		} catch (IOException ioe) {
+			Log.e(LOG_TAG, ioe.getMessage());
+			Toast.makeText(this, "Was unable to encrypt file \'" + fileInName
+					+ "\'.", Toast.LENGTH_SHORT);
+		} catch (NoSuchAlgorithmException e) {
+			Log.e(LOG_TAG, e.getMessage());
+		} catch (NoSuchPaddingException e) {
+			Log.e(LOG_TAG, e.getMessage());
+		} catch (InvalidKeyException e) {
+			Log.e(LOG_TAG, e.getMessage());
+		} finally {
+			if (input != null)
+				try {
+					input.close();
+				} catch (IOException e) {
+					Log.e(LOG_TAG, "Cannot close input stream!");
+					e.printStackTrace();
+				}
+			if (output != null)
+				try {
+					output.close();
+				} catch (IOException e) {
+					Log.e(LOG_TAG, "Cannot close output stream!");
+					e.printStackTrace();
+				}
+		}
+	}// End encryptFile
 
-    public enum failureTypes {
-      KEY_NULL, DATABASE_ERROR, EMPTY_FIELD
-    }
+	private class FileTooBigException extends Exception {
+		static final long serialVersionUID = 1877;
 
-    private failureTypes fail;
+		FileTooBigException(String msg) {
+			super(msg);
+		}
+	}
 
-    KeyGenFailException(failureTypes failureType) {
-      fail = failureType;
-    }
-  }
+	private static class KeyGenFailException extends Exception {
+		static final long serialVersionUID = 1878;
+
+		public enum failureTypes {
+			KEY_NULL, DATABASE_ERROR, EMPTY_FIELD
+		}
+
+		private failureTypes fail;
+
+		KeyGenFailException(failureTypes failureType) {
+			fail = failureType;
+		}
+	}
 }
